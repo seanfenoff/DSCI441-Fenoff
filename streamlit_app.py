a694dcce429b67de3de30e9b8fe0fcd7b60809c7
Sean Fenoff <smfen@Seans-MacBook-Pro.local>
2023-04-30 21:27:32 -0400

    Design things.

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,17 +13,20 @@ import numpy as np
 st.write("Electrocardiogram Arrythmia Detection using Long-Short Term Memory Networks")
 st.write("This app will allow you to choose a testing input and it will output an arrythmia classification")
 
-train = pd.read_csv("mitbih_train.csv", header=None)
-train_y = train.iloc[:, -1]
-train_y = train_y.astype('int')
-train_x = train.iloc[:, :-1]
-train_x = train_x.astype('float')
-unique, counts = np.unique(train_y, return_counts=True)
-print(f'unique values: {unique}')
-print(f'counts: {counts}')
-class_names = {0: 'N', 1: 'S', 2: 'V', 3: 'F', 4: 'Q'}
+# train = pd.read_csv("mitbih_train.csv", header=None)
+# train_y = train.iloc[:, -1]
+# train_y = train_y.astype('int')
+# train_x = train.iloc[:, :-1]
+# train_x = train_x.astype('float')
+# unique, counts = np.unique(train_y, return_counts=True)
+# print(f'unique values: {unique}')
+# print(f'counts: {counts}')
+# class_names = {0: 'N', 1: 'S', 2: 'V', 3: 'F', 4: 'Q'}
 
-if st.checkbox('Show Example Sinus Rythm - N'):
-    chart_data = train_x[1]
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
     chart_data
     
