03d63e83e3f760dc17dd21e8135ef62a5732a593
Sean Fenoff <smfen@Seans-MacBook-Pro.local>
2023-05-02 21:46:52 -0400

    Trying to load in model to app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ Created on Mon Apr 24 10:11:33 2023
 import streamlit as st
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 
 st.write("Electrocardiogram Arrythmia Detection using Long-Short Term Memory Networks")
 st.write("This app will allow you to choose a testing input and it will output an arrythmia classification")
@@ -23,10 +24,48 @@ st.write("This app will allow you to choose a testing input and it will output a
 # print(f'counts: {counts}')
 # class_names = {0: 'N', 1: 'S', 2: 'V', 3: 'F', 4: 'Q'}
 
-if st.checkbox('Show dataframe'):
+if st.checkbox('Show Example plot of Sinus Rythm - N'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    st.line_chart(data=chart_data)
+
+if st.checkbox('Show Example plot of Superventricular Premature - S'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    st.line_chart(data=chart_data)
+
+if st.checkbox('Show Example plot of Ventricular Premature - V'):
     chart_data = pd.DataFrame(
        np.random.randn(20, 3),
        columns=['a', 'b', 'c'])
 
     st.line_chart(data=chart_data)
     
+if st.checkbox('Show Example plot of Ventricular Fusion - F'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    st.line_chart(data=chart_data)
+
+if st.checkbox('Show Example plot of Unclassifiable - Q'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    st.line_chart(data=chart_data)
+    
+uploaded_file = st.file_uploader("Choose input file for classification.")
+
+test_df = pd.read_csv(uploaded_file, header=None)
+# features_df = test_df.iloc[:,:-1]
+# features_df = features_df.astype('float32')
+# X_test_np = features_df.to_numpy()
+# X_test_np = np.reshape(X_test_np, (X_test_np.shape[0], 1, X_test_np.shape[1]))
+
+model = tf.saved_model.load('/Users/smfen/Documents/Lehigh Graduate School/Lehigh Spring 2023/DSCI 441 -- Stat. and ML/Project/streamlit_model')
+model.predict(test_df, verbose=1)
